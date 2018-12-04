f1bddebfd662fe37f399d2442ad54d75de8c7dcc
Ross Smith II <ross@smithii.com>
2018-12-04 11:56:26 -0800

    Break up main into functions

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path"
 	"path/filepath"
 	"runtime"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/bogem/id3v2"
 	"github.com/gocarina/gocsv"
@@ -17,8 +19,16 @@ import (
 )
 
 const (
+	defaultAlbumNo  = "1"
+	defaultCopyright = "Copyright © & ℗ %d, %s"
+	defaultCopyrightDescription = "Copyright"
+	defaultCSV    	= "default.csv"
+	defaultEncodedBy = ""
+	defaultJPG    	= "default.jpg"
 	defaultLanguage = "eng"
-	defaultImage    = "folder.jpg"
+	defaultMime		= "image/jpeg"
+	defaultTrackNo  = "1"
+	defaultXML    	= "default.xml"
 )
 
 // Track contains the MP3 tags to be updated
@@ -37,6 +47,185 @@ type Track struct { // Our example struct, you can use "-" to ignore a field
 	Year        string `csv:"year,omitempty"`
 }
 
+func findNewestFile(dir string, mask string) (name string, err error) {
+    files, err := ioutil.ReadDir(dir)
+    if err != nil {
+        log.Fatalf("Cannot read directory %s: %s", dir, err)
+    }
+    var modTime time.Time
+    var names []string
+    for _, fi := range files {
+		if mask != "" {
+			matched, err := path.Match(mask, fi.Name())
+			if err != nil {
+				log.Printf("Match failed on %s for %s", mask, fi.Name)
+				return "", err
+			}
+			if ! matched {
+				continue
+			}
+		}
+        if ! fi.Mode().IsRegular() {
+			continue
+		}
+		if fi.ModTime().Before(modTime) {
+			continue
+		}
+		if ! fi.ModTime().After(modTime) {
+			continue
+		}
+		modTime = fi.ModTime()
+		names = names[:0]
+		names = append(names, fi.Name())
+    }
+    if len(names) > 0 {
+        return names[0], nil
+    }
+	return "", fmt.Errorf("No files found matching %s", mask)
+}
+
+func setTrackDefaults(track *Track, lastTrack *Track, year int) bool {
+	if track.Title == "" {
+		log.Printf("Skipping track, as the track title is empty")
+		return false
+	}
+	if track.Artist == "" {
+		track.Artist = lastTrack.Artist
+	}
+	if track.AlbumArtist == "" {
+		track.AlbumArtist = lastTrack.AlbumArtist
+	}
+	if track.AlbumArtist == "" {
+		track.AlbumArtist = track.Artist
+	}
+	if track.AlbumNo == "" {
+		track.AlbumNo = lastTrack.AlbumNo
+	}
+	if track.AlbumNo == "" {
+		track.AlbumNo = defaultAlbumNo
+	}
+	if track.AlbumPrefix == "" {
+		track.AlbumPrefix = lastTrack.AlbumPrefix
+	}
+	if track.AlbumTitle == "" {
+		track.AlbumTitle = lastTrack.AlbumTitle
+	}
+	if track.Copyright == "" {
+		track.Copyright = lastTrack.Copyright
+	}
+	if track.Copyright == "" {
+		track.Copyright = fmt.Sprintf(defaultCopyright, year, track.Artist)
+	}
+	if track.Genre == "" {
+		track.Genre = lastTrack.Genre
+	}
+	if track.No == "" {
+		i, err := strconv.Atoi(lastTrack.No)
+		if err == nil {
+			track.No = strconv.Itoa(i + 1)
+		}
+	}
+	if track.No == "" {
+		track.No = defaultTrackNo
+	}
+	if track.Prefix == "" {
+		track.Prefix = lastTrack.Prefix
+	}
+	if track.Year == "" {
+		track.Year = lastTrack.Year
+	}
+	if track.Year == "" {
+		track.Year = strconv.Itoa(year)
+	}
+	return true
+}
+
+func setTags(tag *id3v2.Tag, track *Track) {
+	//tag.SetDefaultEncoding(id3v2.EncodingUTF8)
+	//tag.SetVersion(4)
+
+	tag.AddTextFrame(tag.CommonID("Band/Orchestra/Accompaniment"), id3v2.EncodingUTF8, track.AlbumArtist)
+	tag.SetAlbum(track.AlbumTitle)
+	tag.AddTextFrame(tag.CommonID("Part of a set"), id3v2.EncodingUTF8, track.AlbumNo)
+	tag.AddTextFrame(tag.CommonID("Album/Movie/Show title"), id3v2.EncodingUTF8, track.AlbumTitle)
+	tag.SetArtist(track.Artist)
+	tag.AddTextFrame(tag.CommonID("Copyright message"), id3v2.EncodingUTF8, track.Copyright)
+	//panics:
+	//tag.AddTextFrame(tag.CommonID("Comments"), id3v2.EncodingUTF8, track.Copyright)
+	tag.SetGenre(track.Genre)
+	tag.AddTextFrame(tag.CommonID("Track number/Position in set"), id3v2.EncodingUTF8, track.No)
+	tag.SetTitle(track.Title)
+	tag.SetYear(track.Year)
+
+	// Set comment frame.
+	comment := id3v2.CommentFrame{
+		Encoding:    id3v2.EncodingUTF8,
+		Language:    defaultLanguage,
+		Description: defaultCopyrightDescription,
+		Text:        track.Copyright,
+	}
+	tag.AddCommentFrame(comment)
+
+	tag.AddTextFrame(tag.CommonID("Composer"), id3v2.EncodingUTF8, track.Artist)
+	if defaultEncodedBy != "" {
+		tag.AddTextFrame(tag.CommonID("Encoded by"), id3v2.EncodingUTF8, defaultEncodedBy)
+	}
+	tag.AddTextFrame(tag.CommonID("Language"), id3v2.EncodingUTF8, defaultLanguage)
+	tag.AddTextFrame(tag.CommonID("Original filename"), id3v2.EncodingUTF8, track.Filename)
+}
+
+func addFrontCover(filename string, mimeType string) (pic *id3v2.PictureFrame, err error) {
+	_, err = os.Stat(filename)
+	if err != nil {
+		log.Printf("Cannot read %s: %s", filename, err)
+		return nil, nil
+	}
+	
+	// See https://godoc.org/github.com/bogem/id3v2#PictureFrame
+	artwork, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("Cannot read %s: %s", filename, err)
+	}
+
+	pic = &id3v2.PictureFrame{
+		Encoding:    id3v2.EncodingUTF8,
+		MimeType:    mimeType,
+		PictureType: id3v2.PTFrontCover,
+		Description: "Front cover",
+		Picture:     artwork,
+	}
+	return pic, nil
+}
+
+func processTrack(track *Track, lastTrack *Track) {
+	//fmt.Printf("%+v\n", track)
+	log.Printf("Processing %v\n", track.Filename)
+	fi, err := os.Stat(track.Filename)
+	if err != nil {
+		log.Fatalf("Cannot open %s: %s", track.Filename, err)
+	}
+	t := fi.ModTime()
+	year := t.Year()
+	tag, err := id3v2.Open(track.Filename, id3v2.Options{Parse: true})
+	if err != nil {
+		log.Fatalf("Cannot open %s: %s", track.Filename, err)
+	}
+
+	setTrackDefaults(track, lastTrack, year)
+	setTags(tag, track)
+
+	pic, err := addFrontCover(defaultJPG, defaultMime)
+	if pic != nil {
+		tag.AddAttachedPicture(*pic)
+	}
+	// Write it to file.
+	err = tag.Save()
+	if err != nil {
+		log.Fatal("Error while saving a tag: ", err)
+	}
+	tag.Close()
+}
+
 func main() {
 	basename := filepath.Base(os.Args[0])
 	progname := strings.TrimSuffix(basename, filepath.Ext(basename))
@@ -49,141 +238,27 @@ func main() {
 		runtime.NumCPU(),
 		runtime.GOMAXPROCS(-1))
 
-	var filename = "playlist.csv"
+	var filename = defaultCSV
 	if len(os.Args) >= 2 {
 		filename = os.Args[1]
 	}
 	csvFile, err := os.Open(filename)
 	if err != nil {
-		panic(err)
+		log.Panicf("Cannot open %s: %s", filename, err)
 	}
 	defer csvFile.Close()
 
 	tracks := []*Track{}
 
-	if err := gocsv.UnmarshalFile(csvFile, &tracks); err != nil { // Load track from file
-		panic(err)
+	err = gocsv.UnmarshalFile(csvFile, &tracks)
+	if err != nil { // Load track from file
+		log.Panicf("Cannot read %s: %s", filename, err)
 	}
+
 	lastTrack := &Track{}
 
 	for _, track := range tracks {
-		fmt.Printf("%+v\n", track)
-		log.Printf("Processing %v\n", track.Filename)
-		fi, err := os.Stat(track.Filename)
-		if err != nil {
-			log.Fatalf("Cannot open %s: %s", track.Filename, err)
-		}
-		t := fi.ModTime()
-		year := t.Year()
-		tag, err := id3v2.Open(track.Filename, id3v2.Options{Parse: true})
-		if err != nil {
-			log.Fatalf("Cannot open %s: %s", track.Filename, err)
-		}
-		defer tag.Close()
-
-		if track.Artist == "" {
-			track.Artist = lastTrack.Artist
-		}
-		if track.AlbumArtist == "" {
-			track.AlbumArtist = lastTrack.AlbumArtist
-		}
-		if track.AlbumArtist == "" {
-			track.AlbumArtist = track.Artist
-		}
-		if track.AlbumNo == "" {
-			track.AlbumNo = lastTrack.AlbumNo
-		}
-		if track.AlbumNo == "" {
-			track.AlbumNo = "1"
-		}
-		if track.AlbumPrefix == "" {
-			track.AlbumPrefix = lastTrack.AlbumPrefix
-		}
-		if track.AlbumTitle == "" {
-			track.AlbumTitle = lastTrack.AlbumTitle
-		}
-		if track.Copyright == "" {
-			track.Copyright = lastTrack.Copyright
-		}
-		if track.Copyright == "" {
-			track.Copyright = fmt.Sprintf("Copyright © & ℗ %d, %s", year, track.Artist)
-		}
-		if track.Genre == "" {
-			track.Genre = lastTrack.Genre
-		}
-		if track.No == "" {
-			i, err := strconv.Atoi(lastTrack.No)
-			if err == nil {
-				track.No = strconv.Itoa(i + 1)
-			}
-		}
-		if track.No == "" {
-			track.No = "1"
-		}
-		if track.Prefix == "" {
-			track.Prefix = lastTrack.Prefix
-		}
-		if track.Title == "" {
-			log.Printf("title is empty")
-			continue
-		}
-		if track.Year == "" {
-			track.Year = lastTrack.Year
-		}
-		if track.Year == "" {
-			track.Year = strconv.Itoa(year)
-		}
-
-		//tag.SetDefaultEncoding(id3v2.EncodingUTF8)
-		//tag.SetVersion(4)
-
-		tag.AddTextFrame(tag.CommonID("Band/Orchestra/Accompaniment"), id3v2.EncodingUTF8, track.AlbumArtist)
-		tag.SetAlbum(track.AlbumTitle)
-		tag.AddTextFrame(tag.CommonID("Part of a set"), id3v2.EncodingUTF8, track.AlbumNo)
-		tag.AddTextFrame(tag.CommonID("Original filename"), id3v2.EncodingUTF8, track.Filename)
-		tag.AddTextFrame(tag.CommonID("Album/Movie/Show title"), id3v2.EncodingUTF8, track.AlbumTitle)
-		tag.SetArtist(track.Artist)
-		tag.AddTextFrame(tag.CommonID("Copyright message"), id3v2.EncodingUTF8, track.Copyright)
-		//panic:
-		//tag.AddTextFrame(tag.CommonID("Comments"), id3v2.EncodingUTF8, track.Copyright)
-		tag.SetGenre(track.Genre)
-		tag.AddTextFrame(tag.CommonID("Track number/Position in set"), id3v2.EncodingUTF8, track.No)
-		tag.SetTitle(track.Title)
-		tag.SetYear(track.Year)
-
-		tag.AddTextFrame(tag.CommonID("Composer"), id3v2.EncodingUTF8, track.Artist)
-		tag.AddTextFrame(tag.CommonID("Encoded by"), id3v2.EncodingUTF8, "")
-
-		tag.AddTextFrame(tag.CommonID("Language"), id3v2.EncodingUTF8, defaultLanguage)
-		// Set comment frame.
-		comment := id3v2.CommentFrame{
-			Encoding:    id3v2.EncodingUTF8,
-			Language:    "eng",
-			Description: "Copyright",
-			Text:        track.Copyright,
-		}
-		tag.AddCommentFrame(comment)
-		_, err = os.Stat(defaultImage)
-		if err == nil {
-			// See https://godoc.org/github.com/bogem/id3v2#PictureFrame
-			artwork, err := ioutil.ReadFile(defaultImage)
-			if err != nil {
-				log.Fatalf("Cannot read %s: %s", defaultImage, err)
-			}
-
-			pic := id3v2.PictureFrame{
-				Encoding:    id3v2.EncodingUTF8,
-				MimeType:    "image/jpeg",
-				PictureType: id3v2.PTFrontCover,
-				Description: "Front cover",
-				Picture:     artwork,
-			}
-			tag.AddAttachedPicture(pic)
-		}
-		// Write it to file.
-		if err = tag.Save(); err != nil {
-			log.Fatal("Error while saving a tag: ", err)
-		}
+		processTrack(track, lastTrack)
 		lastTrack = track
 	}
 }
